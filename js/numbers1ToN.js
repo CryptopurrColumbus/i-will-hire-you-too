2eb19ed10e5e25388e4601ac1a7d8df29ff220bf
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-05-14 23:42:39 +0530

    improved code sum of numbers one to n
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- js/numbers1ToN.js
+++ js/numbers1ToN.js
@@ -1,6 +1,4 @@
 var rl = require("readline-sync");
-var number = rl.question('Please enter a number: ');
-number = parseInt(number);
 
 function numbersToN(number) {
     var sum = 0;
@@ -10,16 +8,15 @@ function numbersToN(number) {
     return sum;
 }
 
-if (isNaN(number)) {
-    while (true) {
-        console.log ("you havent returned a number");
-        number = rl.question('Please enter a number: ');
-        number = parseInt(number);
-        if (!isNaN(number)){
-            break;
-        }
-    
+var number;
+while (true) {
+    number = rl.question('Please enter a number: ');
+    number = parseInt(number);
+    if (!isNaN(number)){
+        break;
     }
+    console.log ("You havent returned a number!");
 }
+
 var sum = numbersToN(number);
 console.log(sum);
