57f3288fc5a2d6a7a8a4172466735197af62e059
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-05-19 22:54:43 +0530

    improve primeNumber.js
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- js/primeNumber.js
+++ js/primeNumber.js
@@ -6,41 +6,37 @@ let number;
 while (true) {
     number = rl.question('Please enter a whole number:');
     number = parseInt(number);
-    if (!isNaN(number) && number >=0){
+    if (!isNaN(number) && number >= 0){
         break;
     }
     console.log('You haven\'t entered a whole number.');
 }
 
 function primenumbers(n) {
-    let primenumberlist= [];
-    for (a = 1; a <= n; a += 1 ) {
-       var x = primeOrNot(a);
-       if (x === true ) {
-        primenumberlist.push(a);
+    let primenumberlist = [];
+    for (let a = 1; a <= n; a += 1) {
+       var isprime = primeOrNot(a);
+       if (isprime) {
+           primenumberlist.push(a);
        }
     }
     return primenumberlist;
 }
 
 function primeOrNot(n) {
-    let divisorNo =[];
-    for (let a=1; a<=n; a += 1) {
-         if (n%a=== 0) {
+    let divisorNo = [];
+    for (let a = 1; a <= n; a += 1) {
+         if (n % a === 0) {
              divisorNo.push(a);
          }
     }
-    if (divisorNo.length === 2) {
-        return true;
-    } else {
-        return false;
-    }
+    return divisorNo.length === 2;
 }
 
 let prime = primenumbers(number);
 if (prime.length === 0){
     console.log(`There are no prime numbers till ${number}.`);
 } else {
-console.log(`Prime no.s till ${number} are : ${prime}.`);
+    console.log(`Prime no.s till ${number} are : ${prime}.`);
 }
 
