4fa69a2d8f3a10c14a045c52fa89bd9ac39dc3da
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-05-19 18:02:51 +0530

    fix typo in test_readline.js
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- js/test_readline.js
+++ js/test_readline.js
@@ -2,4 +2,4 @@ var rl = require ("readline-sync");
 // is a module so has to be writtenls only once for any no of user inputs
 var abc = rl.question('Enter a number: ');
 abc = parseInt(abc);
-console.log (typeof(abc))
+console.log (typeof(abc));
