824270a77758b922ab20f555251e884ea08f375c
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-05-20 22:09:50 +0530

    improve code leapYear.js
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- js/leapYear.js
+++ js/leapYear.js
@@ -10,26 +10,32 @@ while (true) {
     }
     console.log("You havent entered a whole number.");
 }
+
 function firstYear() {
-    let d = new Date();
-    let n = d.getFullYear();
-    if (n % 4 === 0) { 
-        n += 4;
-    }else if (n % 4 === 1) {
-        n += 3;
-    }else if (n % 4 === 2) {
-        n += 2;
-    }else if (n % 4 === 3) {
-        n += 1;
+    let currentDate = new Date();
+    let firstLeapYear = currentDate.getFullYear();
+    if (firstLeapYear % 4 === 0) { 
+        firstLeapYear += 4;
+    } else if (firstLeapYear % 4 === 1) {
+        firstLeapYear += 3;
+    } else if (firstLeapYear % 4 === 2) {
+        firstLeapYear += 2;
+    } else if (firstLeapYear % 4 === 3) {
+        firstLeapYear += 1;
     }
-    return n;
+    return firstLeapYear;
 }
+
 let firstY = firstYear();
 
 function nextNLeapYears (n) {
-    console.log(`The next ${number} leap years are: `);
-    for (let a = 0 ; a <= n-1; a+=1) {
-        console.log(firstY+4*a);
+    
+    let leapyear = [];
+    for (let a = 0; a <= n-1; a += 1) {
+        leapyear.push(firstY + 4 * a);
     }
+    return leapyear;
 }
-nextNLeapYears(number);
+
+let list = nextNLeapYears(number);
+console.log(`Next ${number} leap years are: ${list}`);
