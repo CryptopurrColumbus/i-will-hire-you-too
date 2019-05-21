ade9aeee28887a71886856de6e48631184c9cd7b
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-05-22 00:08:46 +0530

    run game without endline for five times
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- js/guessRandomNo.js
+++ js/guessRandomNo.js
@@ -10,36 +10,62 @@
 
 const rl = require("readline-sync");
 
-// Courtesy MDN
-function getRandomNo(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+let trys =[];
 
-//The maximum is exclusive and the minimum is inclusive
-randomNo = getRandomNo(1,201);
-console.log(randomNo);
+function oneGo()  {
+    
 
-
-function getUserInput() {
- let userInput = rl.question('Guess the no between 1 and 200:');
- userInput = parseInt(userInput);
- return userInput;
-}
-
-let a = getUserInput();
-
-
- if (a !== randomNo) {
-    if (a>randomNo) {
-        console.log('Your number is too large.');
-    } else if (a<randomNo) {
-        console.log('Your number is too small.')
+    // Courtesy MDN
+    function getRandomNo(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
     }
-} else {
-    console.log('Yayee');
+
+    //The maximum is exclusive and the minimum is inclusive
+    randomNo = getRandomNo(1,201);
+    console.log(randomNo);
+
+    function getUserInput() {
+        let userInput = rl.question('Guess the no between 1 and 200:');
+        userInput = parseInt(userInput);
+        return userInput;
+    }
+
+   let a = getUserInput();
+            
+
+    if (a !== randomNo) {
+        if (a>randomNo) {
+            console.log('Your number is too large.');
+        } else if (a<randomNo) {
+            console.log('Your number is too small.')
+        }
+    } else {
+        console.log('Yayee');
+
+    }
+    return a;
 }
+
+for (let x = 1; x <= 5; x += 1) {
+trys.push(oneGo());
+if (trys[trys.length-1]===trys[trys.length-2]) {
+        trys.pop();
+}
+console.log(`trys array 2-${trys}`);
+
+
+ }
+ console.log (trys.length);
+
+
+
+
+
+
+
+ 
 
 
 
