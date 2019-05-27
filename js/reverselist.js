c41aaa141a74f2aaedbfc600e38a9cf6aceb504c
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-05-27 21:17:11 +0530

    reverselist.js: reverse a list
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

---
+++ js/reverselist.js
@@ -0,0 +1,40 @@
+// Write function that reverses a list
+
+const rl = require('readline-sync');
+
+function userInputIntoArray() {
+	let numbers;
+	let numarray;
+	while (true) {
+		numbers = rl.question('please enter numbers separated by commas: ');
+
+		numbersarray = numbers.split(',');
+		numarray = numbersarray.map(ele => parseFloat(ele));
+		if (
+			Array.isArray(numarray) &&
+			numarray.length > 0 &&
+			numarray.every(checkForNo)
+		) {
+			break;
+		}
+		console.log("You haven't entered numbers separated by commas.");
+	}
+	return numbersarray;
+}
+
+function reversesarray(arrinput) {
+	let reversedarr = [];
+	for (var i = arrinput.length - 1; i >= 0; i -= 1) {
+		reversedarr.push(arrinput[i]);
+	}
+	return reversedarr;
+}
+
+function checkForNo(a) {
+	return !isNaN(a);
+}
+
+let arrinput = userInputIntoArray();
+console.log(arrinput);
+let output = reversesarray(arrinput);
+console.log(`The array ${arrinput} reversed is ${output}.`);
