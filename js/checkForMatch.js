8af97862373ae45fe5e2179397f9e99b3cdde625
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-05-27 23:37:26 +0530

    checkForMatch.js: Check if number exists in array
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

---
+++ js/checkForMatch.js
@@ -0,0 +1,47 @@
+// Write a function that checks whether an element occurs in a list.
+const rl = require('readline-sync');
+
+function userInputIntoArray() {
+	let numbers;
+	let numarray;
+	while (true) {
+		numbers = rl.question('please enter numbers separated by commas: ');
+
+		numbersarray = numbers.split(',');
+		numarray = numbersarray.map(ele => parseFloat(ele));
+		if (
+			Array.isArray(numarray) &&
+			numarray.length > 0 &&
+			numarray.every(checkForNo)
+		) {
+			break;
+		}
+		console.log("You haven't entered numbers separated by commas.");
+	}
+	return numarray;
+}
+
+function checkForNo(a) {
+	return !isNaN(a);
+}
+function checkNoExistence(ip, arrinput) {
+   
+	let indexof = arrinput.indexOf(ip);
+	return indexof !== -1;t
+}
+
+let arrinput = userInputIntoArray();
+
+function takeInputToCheck() {
+	let element = rl.question('please enter seach number: ');
+	element = parseFloat(element);
+	return element;
+}
+
+let ip = takeInputToCheck();
+
+if (checkNoExistence(ip, arrinput)) {
+	console.log('The element exists in the list.');
+} else {
+	console.log('The element doesnt exist in the list.');
+}
