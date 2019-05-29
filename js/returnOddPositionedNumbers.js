9b63d42312024ca65eedebd5d2e3a68af0786b4b
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-05-29 21:21:34 +0530

    returnOddPositionedNumbers.js: store value of array length  in var
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- js/returnOddPositionedNumbers.js
+++ js/returnOddPositionedNumbers.js
@@ -8,7 +8,7 @@ function userInputIntoArray() {
 	let numarray;
 	while (true) {
 		numbers = rl.question('Please enter numbers separated by commas: ');
-        numbersarray = numbers.split(',');
+		numbersarray = numbers.split(',');
 		numarray = numbersarray.map(ele => parseFloat(ele));
 		if (
 			Array.isArray(numarray) &&
@@ -26,15 +26,15 @@ function checkForNo(a) {
 	return !isNaN(a);
 }
 
-let arrinput = userInputIntoArray();
-
-function genArrOfOddPosElems (arrinput) {
-    let oddPosArr =[];
-    for (let a = 1; a <= arrinput.length -1; a +=2) {
-         oddPosArr.push(arrinput[a]);
-    }
-    return oddPosArr;
+function genArrOfOddPosElems(arrinput) {
+	let oddPosArr = [];
+	const arrlength = arrinput.length;
+	for (let a = 1; a < arrlength; a += 2) {
+		oddPosArr.push(arrinput[a]);
+	}
+	return oddPosArr;
 }
 
+let arrinput = userInputIntoArray();
 let output = genArrOfOddPosElems(arrinput);
 console.log(`Numbers on odd positions of ${arrinput} are ${output}.`);
